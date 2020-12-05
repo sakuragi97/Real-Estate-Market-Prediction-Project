a16a8f77b7f0b947e7db9cd3074f799a3403264d
Sakuragi97 <yasser.elbachiri@gmail.com>
2020-12-05 03:44:50 +0100

    Implementing Flask server & APIs

---
+++ server/Server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from server import Util
+
+app = Flask(__name__)
+
+
+@app.route('/getLocationNames')
+def getLocationNames():
+    response = jsonify({
+        'locations': Util.getLocationNames()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/predict_price', methods=['POST'])
+def predictPrice():
+    location = request.form['location']
+    sqmeter = float(request.form['total_sqmeter'])
+    bath = int(request.form['bath'])
+    bhk = int(request.form['bhk'])
+
+    response = jsonify({
+        'estimated_price' : Util.getEstimatedPrice(location, sqmeter, bath, bhk)
+    })
+    return response
+
+
+if __name__ == '__main__':
+    print('Starting python Flask server for - Real Estate Market Price Prediction...')
+    app.run(debug=True)
---
+++ server/Util.py
@@ -0,0 +1,48 @@
+import json
+import pickle
+import numpy as np
+
+__columns = None
+__locations = None
+__model = None
+
+
+def getLocationNames():
+    loadArtifacts()
+    return __locations
+
+
+def loadArtifacts():
+    print('Loading artifacts...')
+    global __columns
+    global __locations
+    global __model
+    with open('./artifacts/columns.json', 'r') as f:
+        __columns = json.load(f)['data_columns']
+    __locations = __columns[3:]
+    with open('artifacts/Bengaluru_House_Data_Model.pickle', 'rb') as f:
+        __model = pickle.load(f)
+    print('Loading artifacts Done!')
+
+
+def getEstimatedPrice(location, sqmeter, bath, bhk):
+    try:
+        loc_index = __columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__columns))
+    x[0] = bhk
+    x[1] = sqmeter
+    x[2] = bath
+    if loc_index >= 0:
+        x[loc_index] = 1
+    return round(__model.predict([x])[0], 2)
+
+
+if __name__ == '__main__':
+    print(getLocationNames())
+    print(getEstimatedPrice('1st Phase JP Nagar', 100, 2, 2))
+    print(getEstimatedPrice('1st Phase JP Nagar', 100, 2, 3))
+    print(getEstimatedPrice('1st Phase JP Nagar', 120, 2, 4))
+    print(getEstimatedPrice('Whitefield', 160, 3, 4))
